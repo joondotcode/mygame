10cd2968e63b60160d4d99df68cfa5c51fc57b78
s1nstyle <48565890+Bamasdf@users.noreply.github.com>
2020-12-17 19:06:41 -0500

    Added Helicopter Game 3D

---
+++ Assets/Resources/Scripts/Gem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -25)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        transform.Rotate(0, 5f, 0, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent.GetComponent<HeliController>().PickupGem();
+        Destroy(gameObject);
+    }
+}
